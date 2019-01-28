6e501155ce80a14158d9dd4d61b84b5875352997
Jaume Mussons <jaume.mussons@edertone.com>
2019-01-28 20:15:24 +0100

    Renamed post params to post data

--- TurboSite-Php/src/main/php/model/WebService.php
+++ TurboSite-Php/src/main/php/model/WebService.php
@@ -23,30 +23,35 @@ class WebService{
 
 
     /**
-     * Defines if the web service accepts POST parameters. Note that when using POST, all the service parameters must be sent
-     * inside a single JSON object that is passed via the "params" POST variable.
+     * Defines if the web service accepts POST parameters. Note that when using POST, all the service parameters must be passed through
+     * a POST variable called "data". We can pass there a single string or a JSON object containing all the service parameter values we
+     * want.
      *
-     * The variable "params" is the only POST value that will be accepted by the service. All the data to the service must be passed
+     * The variable "data" is the only POST value that will be accepted by the service. All the data to the service must be passed
      * via this variable. Any other POST variable that is sent to the service will make it fail.
      */
-    public $enablePostParams = false;
+    public $enablePostData = false;
 
 
     /**
      * Defines how many GET parameters are accepted by this service. Anyones beyond this limit will make the service fail.
-     * If a service has a missing value for any of the enabled parameters and there's no default value defined, the service fail.
+     * If a service has a missing value for any of the enabled parameters and there's no default value defined, the service will fail.
+     *
+     * Get parameters on http services can only be passed in the form .../api/../../service-name/param1/param2/param3/... The standard way
+     * to encode GET parameter in urls (?param1=v1&param2=v2...) is not accepted and will be ignored.
      */
     public $enabledGetParams = 0;
 
 
     /**
-     * Stores the actual values of the GET parameters that have been passed to this service via the URL
+     * Stores the actual values of the GET parameters that have been passed to this service via the URL, sorted in the same order as specified
+     * in the url
      */
     private $_receivedURLGetParameters = [];
 
 
     /**
-     * Stores the actual values of the GET parameters that have been passed to this service via the URL
+     * Stores the number of GET parameters that have been passed to this service via the URL
      */
     private $_receivedURLGetParametersCount = 0;
 
@@ -58,7 +63,7 @@ class WebService{
 
 
     /**
-     * Contains the value for the current service url URI fragment but splitted as an array
+     * Contains the value for the current service url URI fragment just after https://.../api/ but splitted as an array
      * where each element is a URI fragment (fragments are divided by /).
      */
     private $_URIElements = [];
@@ -66,8 +71,11 @@ class WebService{
 
     /**
      * Class constructor
+     *
+     * @param string $data If we create this service via code we can pass the POST $data here and it will be loaded
+     *        by the service as if it was passed via HTTP POST
      */
-    public function __construct(){
+    public function __construct($data = null){
 
         $this->setup();
 
@@ -103,16 +111,16 @@ class WebService{
         }
 
         // Check post parameters are valid
-        if(count(array_keys($_POST)) > 0 && !$this->enablePostParams){
+        if(count(array_keys($_POST)) > 0 && !$this->enablePostData){
 
             throw new UnexpectedValueException('Received POST variables but POST not enabled on service');
         }
 
-        $expectedPostVars = isset($_POST['params']) ? 1 : 0;
+        $expectedPostVars = isset($_POST['data']) ? 1 : 0;
 
         if(count(array_keys($_POST)) !== $expectedPostVars){
 
-            throw new UnexpectedValueException('Unexpected POST variables received. Only "params" variable is accepted');
+            throw new UnexpectedValueException('Unexpected POST variables received. Only "data" variable is accepted');
         }
     }
 
@@ -149,25 +157,34 @@ class WebService{
 
     /**
      * Get the POST parameters that have been passed to this service.
+     * All the information that is required by the service must be passed or encoded here.
      *
-     * @return string|null The value of the received POST "params" variable as a string (which should be passed through
-     *                     json_decode if necessary) or null if no "params" variable has been passed to the service.
+     * @return string|null The value of the received POST "data" variable as a string (which should be passed through
+     *                     json_decode if necessary) or null if no "data" variable has been passed to the service.
      */
-    public function getPostParams(){
+    public function getPostData(){
 
-        if(isset($_POST['params'])){
+        if(isset($_POST['data'])){
 
-            return $_POST['params'];
+            return $_POST['data'];
         }
 
         return null;
     }
 
 
+    public function getPostDataJSONEncoded(){
+
+        // TODO
+        
+        return null;
+    }
+
+
     /**
-     * Override this method to setup the service parameters
+     * Override this method to setup the service
      */
-    public function setup(){
+    protected function setup(){
 
         // Override this method to modify the setup of your service
     }
