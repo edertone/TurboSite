cc5788747f32080119a1ab6dbaa7423c37c545e9
Jaume Mussons <jaume.mussons@edertone.com>
2019-09-10 18:41:04 +0200

    Fixed autoloader and turbobuilder.json

--- TurboSite-Php/src/test/php/autoloader.php
+++ TurboSite-Php/src/test/php/autoloader.php
@@ -1,17 +1,8 @@
 <?php
 
-/**
- * TurboSite is a web site creation framework.
- *
- * Website : -> http://www.turboframework.org
- * License : -> Licensed under the Apache License, Version 2.0. You may not use this file except in compliance with the License.
- * License Url : -> http://www.apache.org/licenses/LICENSE-2.0
- * CopyRight : -> Copyright 2018 Edertone Advanded Solutions. http://www.edertone.com
- */
 
-
-require_once __DIR__.'/resources/libs/turbocommons-php-3.0.0.phar';
-require_once __DIR__.'/resources/libs/turbodepot-php-5.0.1.phar';
+require_once __DIR__.'/../libs/turbocommons-php-3.0.0.phar';
+require_once __DIR__.'/../libs/turbodepot-php-5.0.1.phar';
 require_once __DIR__.'/../../main/php/autoloader.php';
 
 
@@ -22,12 +13,12 @@ spl_autoload_register(function($className){
     $classPath = str_replace('\\', DIRECTORY_SEPARATOR, str_replace('/', DIRECTORY_SEPARATOR, $className));
 
     // Remove unwanted classname path parts
-    $classPath = explode('src'.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR.'php'.DIRECTORY_SEPARATOR, $classPath);
+    $classPath = explode('src'.DIRECTORY_SEPARATOR.'test'.DIRECTORY_SEPARATOR, $classPath);
     $classPath = array_pop($classPath).'.php';
 
-    if(file_exists(__DIR__.DIRECTORY_SEPARATOR.$classPath)){
+    if(file_exists(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.$classPath)){
 
-        require_once __DIR__.DIRECTORY_SEPARATOR.$classPath;
+        require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.$classPath;
     }
 });
 
